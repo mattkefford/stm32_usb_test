e94f420bd0ac20f28db078af7ac015f1c3aeb9aa
MattKefford <kefford7@gmail.com>
2020-08-03 17:15:44 +0100

    Added instructions to crc lib header

--- Core/Inc/crc16.h
+++ Core/Inc/crc16.h
@@ -1,3 +1,8 @@
+// Module to calculate the CRC16 checksum
+// Add the .h/.c files to your project
+// Call the init function first, in your main
+// When you need a checksum, pass a buffer and length
+// to the bit_by_bit_fast function
 
 int crc16_init_saga_settings(void);
 int crc16_init(int order, unsigned long poly, int direct, unsigned long crcinit, unsigned long crcxor, int refin, int refout);
